24be9e79c3aff4ceaca1f9212d37e543127f3666
Joel Mwala <joelmwala7@gmail.com>
2024-04-19 20:27:50 +0100

    make capitalize work

--- lab5/question6.cpp
+++ lab5/question6.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
 #include<fstream>
 #include<string>
+#include<vector>
 using namespace std;
 
 int vowelsCount(string str);
 int wordsCount(string str);
+
+
 string reverseWords(string str);
+string reverseRecursion(string str);
+
+
 string capitalizeSecond(string str);
 
 int wordsCount2(string str);
@@ -22,14 +28,8 @@ int main(){
         cout<<"og message: "<< fileData<<endl;
         cout<<"Number of vowels: "<<vowelsCount(fileData)<<endl;
         cout<<"Number of words: "<<wordsCount(fileData)<<endl;
-
+        cout<<"Number of words: "<<reverseWords(fileData)<<endl;
     }
-
-    //calculate number of vowels > output
-
-    //calculate number of words > output
-
-    //reverse test file > output
 
     //capitalize second letter of each word > output
     return 0;
@@ -86,3 +86,44 @@ int wordsCount(string str){
 
     return counter;
 }
+
+string reverseRecursion(string str){
+    if (str == ""){
+        return "";
+    }else{
+        return reverseRecursion(str.substr(1)) + str[0];
+    }
+}
+
+string reverseWords(string str){
+    string output = "";
+    int i = 0;
+    while(i < str.length()){
+        output = output + str[str.length() - 1 - i];
+        i++;
+    }
+    return output + "\0";
+}
+
+
+
+
+string capitalizeSecond(string str){
+    string result = "";
+
+    bool flag = false;
+
+    for(int i = 0; i < str.length();i++){
+        if(str[i] != ' ' && flag == false){
+            flag = true;
+            // counter++;
+        }
+
+        if(str[i] == ' '){
+            flag = false;
+        }
+    }
+
+    return "";
+}
+
