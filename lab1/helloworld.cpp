30833370e35fd4cbb0ed60c00678ecac8be2fd94
Joel Mwala <joelmwala7@gmail.com>
2024-05-06 16:16:23 +0100

    add helloworld.cpp

--- lab1/helloworld.cpp
+++ lab1/helloworld.cpp
@@ -1,1 +1,16 @@
 // write hello world program
+#include<iostream>
+#include<vector>
+#include<string>
+
+using namespace std;
+
+int main(){
+    vector<string> msg{"Hello", "c++", "World", "from", "VS code"};
+
+    for(const string &word: msg){
+        cout<< word <<" ";
+    }
+    cout <<endl;
+
+}
