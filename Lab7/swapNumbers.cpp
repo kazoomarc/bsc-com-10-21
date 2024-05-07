9c9be91c30688a2b702c746fb4eb48266114798d
Joel Mwala <joelmwala7@gmail.com>
2024-05-07 07:52:44 +0100

    lab 8

---
+++ Lab7/swapNumbers.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void swapNumbers(int &a, int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main(){
+
+    int varA = 25;
+    int varB = 100;
+
+    cout<<"varA before swap: "<<varA <<endl; //varA is 25
+    cout<<"varB before swap: "<<varB <<endl; //varb is 100
+
+
+    swapNumbers(varA, varB);
+
+    cout<<"varA after swap: "<<varA <<endl;
+    cout<<"varB after swap: "<<varB <<endl;
+
+    return 0;
+}
